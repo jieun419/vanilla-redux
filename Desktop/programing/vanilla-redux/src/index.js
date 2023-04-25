469f920d408405fc8c843a690de4dc7188e186d9
jieun419 <crg1050@naver.com>
2023-04-25 11:24:58 +0900

    js 소스 개선

--- Desktop/programing/vanilla-redux/src/index.js
+++ Desktop/programing/vanilla-redux/src/index.js
@@ -4,15 +4,19 @@ const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 number.textContent = 0
 
 const countModifier = (count = 0, action) => {
-  if(action.type === 'ADD'){
-    return count + 1;
-  }else if(action.type === 'MINUS'){
-    return count - 1;
-  }else{
-    return count;
+  switch(action.type){
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,11 +29,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: 'ADD' })
+  countStore.dispatch({ type: ADD })
 }
 
 const handleMinus = () => {
-  countStore.dispatch({ type: 'MINUS'  })
+  countStore.dispatch({ type: MINUS  })
 }
 
 add.addEventListener('click', handleAdd)
